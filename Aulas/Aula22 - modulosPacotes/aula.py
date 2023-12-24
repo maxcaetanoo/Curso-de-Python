d49528dded0943b9aa1cc9d04df9ea6503938de5
maxcaetanoo <maxsuelcaetano@live.com>
2023-12-24 08:33:49 -0300

    Aula 22

---
+++ Aulas/Aula22 - modulosPacotes/aula.py
@@ -0,0 +1,11 @@
+# Aula
+import module
+
+num = int(input('Digite um número: '))
+fat = module.fatorial(num)
+dob = module.dobro(num)
+tri = module.triplo(num)
+
+print(f'O fatorial de {num} é  {fat}')
+print(f'O dobro de {num} é {dob}')
+print(f'O triplo de {num} é {tri}')
---
+++ Aulas/Aula22 - modulosPacotes/module.py
@@ -0,0 +1,18 @@
+# Modulo
+
+
+def fatorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+
+def dobro(n):
+    d = n * 2
+    return d
+
+
+def triplo(n):
+    t = n * 3
+    return t
