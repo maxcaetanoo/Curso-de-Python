60ceeb0e4fc7ab9b7e568482a2ba30ca7758b3cc
maxcaetanoo <maxsuelcaetano@live.com>
2023-12-26 11:52:51 -0300

    ex108 - import_ajuste

--- Exercicios/ex108/ex108.py
+++ Exercicios/ex108/ex108.py
@@ -1,6 +1,6 @@
 '''Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
 Faça também um programa que importe esse módulo e use algumas dessas funções.'''
-import moeda
+from Exercicios.ex108 import moeda
 
 dinheiro = float(input('Digite um valor em R$: '))
 opcao = '.'
