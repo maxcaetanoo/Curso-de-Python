62bfd589c84459169c7f9d4a64b5c6b29f8124dc
maxcaetano <maxsuelcaetano@live.com>
2024-01-04 21:28:55 -0300

    jogoDaVelha completed

--- Exercicios/jogoDaVelha.py
+++ Exercicios/jogoDaVelha.py
@@ -15,53 +15,58 @@ def menu(text):
 
 
 def gaming(game, select, pc):
+    print('\n')
+    print('-'*17)
     for i, v in enumerate(game):
         if v == '0':
-            if i not in '258':
-                print(f'{i}|', end='')
+            if str(i) not in '258':
+                print(f'{str(i):^5}|', end='')
             else:
-                print(f'{i}\n', end='')
+                print(f'{str(i):^5}\n', end='')
                 for l in range(0,3):
-                    print(f'_', end='') if l != 2 else print(f'{"_"}\n', end='')
+                    print(f'-'*5, end='') if l != 2 else print(f'{"-"*7}\n', end='')
         elif v == select or v == pc:
-            if i not in '258':
-                print(f'{v}|', end='')
+            if str(i) not in '258':
+                print(f'\033[32m{v:^5}\033[m|', end='') if v == select else print(f'\033[31m{v:^5}\033[m|', end='')
             else:
-                print(f'{v}\n', end='')
+                print(f'\033[32m{v:^5}\033[m\n', end='') if v == select else print(f'\033[31m{v:^5}\033[m\n', end='')
                 for l in range(0,3):
-                    print(f'_', end='') if l != 2 else print(f'{"_"}\n', end='')
+                    print(f'-'*5, end='') if l != 2 else print(f'{"-"*7}\n', end='')
             
 
 def play(game, select, pc):
     while True:
         gaming(game, select, pc)
-        print('Sua vez de jogar')
-        selection(f'Olá {select} defina onde você irá jogar pelo indice: ', select, pc, game)
-        print('Agora o pc vai jogar')
-        selection('pc', select, pc, game)
-        sleep(1)
+        print('\n')
         win = champ(game, select, pc)
         if win == 1:
             break
+        print('\n')
+        print('Sua vez de jogar')
+        pl = selection(f'Olá {select} defina onde você irá jogar pelo indice: ', game)
+        game[pl] = select
+        print('Agora o pc vai jogar')
+        gm = selection('pc', game)
+        game[gm] = pc
+        sleep(1)
 
 
-def selection(text, select, pc, game):
+def selection(text, game):
     if text != 'pc':
         play = int(input(text))
-        game[play] = select
     else:
         play = randint(0, 8)
-        game[play] = pc
     while game[play] in 'OX':
         if text != 'pc':
             print('Infelizmente esse indice já foi selecionado, tente novamente com outro indice')
             play = int(input(text))
-        else:
+        elif text == 'pc':
             play = randint(0, 8)
+
     if text != 'pc':
-        game[play] = select
+        return play
     else:
-        game[play] = pc
+        return play
     
 
 def champ(game, select, pc):
@@ -74,7 +79,8 @@ def champ(game, select, pc):
         (select in game[0] and select in game[3] and select in  game[6]) or
         (select in game[1] and select in game[4] and select in  game[7]) or
         (select in game[2] and select in game[5] and select in  game[8])):
-        print(f'O {select} venceu o jogo!')
+        camp = f'O \033[32m{select}\033[m venceu o jogo!'
+        print(f'{camp:^40}')
         win = 1
         return win
     elif ((pc in game[0] and pc in game[1] and pc in game[2]) or
@@ -85,7 +91,8 @@ def champ(game, select, pc):
         (pc in game[0] and pc in game[3] and pc in  game[6]) or
         (pc in game[1] and pc in game[4] and pc in  game[7]) or
         (pc in game[2] and pc in game[5] and pc in  game[8])):
-        print(f'O {pc} venceu o jogo!')
+        camp = f'O \033[31m{pc}\033[m venceu o jogo!'
+        print(f'{camp:^40}')
         win = 1
         return win
     else:
