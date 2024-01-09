cfa91d41d17311fb5783229a4be6de55f4788be0
maxcaetano <maxsuelcaetano@live.com>
2024-01-09 18:24:17 -0300

    bugfix/calss_Tarefa

--- Exercicios/toDoList/lib/classes/__init__.py
+++ Exercicios/toDoList/lib/classes/__init__.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 class Tarefa:
-    def __init__(self, title, date=str(datetime.now()), check=0):
+    def __init__(self, title, date=str(datetime.now()), check="0"):
         self.title = title
         self.date = date
         self.check = check
